ea1aa5669e76ee73f33952b512e1ac039f18dd6a
tsykalenko_o <tsykalenko.oleksii@gmail.com>
2023-09-21 11:56:18 +0300

    Newww BooksSeed and add chankById

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
-use Carbon\Carbon;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class BooksSeeder extends Seeder
 {
@@ -16,18 +16,33 @@ class BooksSeeder extends Seeder
     public function run(): void
     {
         $faker = Faker::create();
-        $chunkSize = 1000;
-        $totalData = 100000;
+        $chunkSize = 2500;
+        $totalData = 1000000;
+        $output = new ConsoleOutput();
+        $progress = new ProgressBar($output, $totalData);
+        $progress->setFormat('verbose');
+        $progress->start();
+        $data = [1];
+        $lang = [
+            0 => 'en',
+            1 => 'ua',
+            2 => 'pl',
+            3 => ' de',
+        ];
         for ($i = 0; $i < $totalData; $i += $chunkSize) {
-            for ($q = 0; $q < $chunkSize; $q++) {
-                DB::table('books')->insert([
+            for ($y = 0; $y < 5; $y++) {
+                $data[] = [
                     'name' => $faker->sentence,
                     'year' => $faker->numberBetween(1970, 2023),
-                    'lang' => $faker->languageCode,
+                    'Lang' => $lang[mt_rand(0, 3)],
                     'pages' => $faker->numberBetween(10, 55000),
-                    'category_id' => $faker->numberBetween(1, 200),
-                ]);
+                    'category_id' => $faker->numberBetween(1, 200)
+                ];
+                $progress->advance();
+                DB:: table('books')
+                    ->insert($data);
             }
         }
+        $progress->finish();
     }
 }
